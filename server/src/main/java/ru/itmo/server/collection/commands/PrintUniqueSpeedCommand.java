822b82205a62a75795223a3aa85935e92617a69e
Liza <Lizachig2003@gmail.com>
2022-05-28 13:34:43 +0300

    Теперь команда print_unique_impact_speed работает

--- server/src/main/java/ru/itmo/server/collection/commands/PrintUniqueSpeedCommand.java
+++ server/src/main/java/ru/itmo/server/collection/commands/PrintUniqueSpeedCommand.java
@@ -1,10 +1,8 @@
 package ru.itmo.server.collection.commands;
 
-import ru.itmo.common.model.HumanBeing;
 import ru.itmo.server.collection.dao.DAO;
 
 import java.util.ArrayList;
-import java.util.List;
 
 public class PrintUniqueSpeedCommand implements Command{
     private final DAO arrayDequeDAO;
@@ -15,24 +13,28 @@ public class PrintUniqueSpeedCommand implements Command{
 
     @Override
     public Object execute(Object arguments) {
-//        arguments = "";
-//        List<Integer> uniqueFieldsSpeed = new ArrayList<>();
-//        if (arrayDequeDAO.size() != 0) {
-//        for(HumanBeing human: arrayDequeDAO.getAll()) {
-//            Integer speed = human.getImpactSpeed();
-//            if(!uniqueFieldsSpeed.contains(speed)) {
-//                uniqueFieldsSpeed.add(speed);
-//            } else {
-//                uniqueFieldsSpeed.remove(speed);
-//            }
-//        }
-//        for(Integer element: uniqueFieldsSpeed) {
-//            arguments += element.toString() + "\n";
-//        }
-//        return arguments;
-//    } else {
-//            return "Коллекция пустая.";
-//        }
-        return "Команда в разработке";
+        ArrayList<Integer> indexes = arrayDequeDAO.getAllSQL();
+        ArrayList<Integer> uniqueImpactSpeed = new ArrayList<>();
+        ArrayList<String> humans = new ArrayList<>();
+        if (indexes.size() != 0) {
+            for(int i : indexes) {
+                String human = arrayDequeDAO.getSQL("id = ", i).get(0);
+                int index_start = human.indexOf("impactSpeed = ") + 14;
+                int index_end = human.indexOf("\n", index_start);
+                int value = Integer.parseInt(human.substring(index_start, index_end));
+                if(uniqueImpactSpeed.contains(value)) {
+                    uniqueImpactSpeed.remove(new Integer(value));
+                } else {
+                    uniqueImpactSpeed.add(value);
+                }
+            }
+            for(Integer speed: uniqueImpactSpeed) {
+                humans.add(arrayDequeDAO.getSQL("impactSpeed = ", speed).get(0));
+            }
+        } else {
+            return "Коллекция пустая.";
+        }
+        return humans.stream().
+                reduce("", (sum, m) -> sum += m + "\n\n", (sum1, sum2) -> sum1 + sum2).trim();
     }
 }
