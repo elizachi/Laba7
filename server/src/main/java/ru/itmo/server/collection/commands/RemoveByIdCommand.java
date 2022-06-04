0313a0c7e25dd42c86089ff974403ef0dd8ff574
apchhiv <ira.karaseva.andre@gmail.com>
2022-06-04 10:17:47 +0300

    fixed message for RemoveByIdCommand

--- server/src/main/java/ru/itmo/server/collection/commands/RemoveByIdCommand.java
+++ server/src/main/java/ru/itmo/server/collection/commands/RemoveByIdCommand.java
@@ -7,18 +7,22 @@ import ru.itmo.server.collection.dao.ArrayDequeDAO;
 import ru.itmo.server.collection.dao.DAO;
 import ru.itmo.server.collection.dao.PostgreSqlDao;
 
-public class RemoveByIdCommand implements Command{
+public class RemoveByIdCommand implements Command {
     private final DAO postgresqlDAO = new PostgreSqlDao();
+
     @Override
     public Response execute(Object arguments, User user) {
         HumanBeing humanBeing = (HumanBeing) arguments;
         int id = humanBeing.getId();
 
-        if(postgresqlDAO.delete(id, user)) {
-            ArrayDequeDAO.getInstance().delete(id, user);
-            return new Response(Response.Status.OK, "remove_by_id: Элемент с id = "+id+" успешно удалён",new User("", ""));
+        if (postgresqlDAO.delete(id, user)) {
+            if (ArrayDequeDAO.getInstance().delete(id, user)) {
+                return new Response(Response.Status.OK, "remove_by_id: Элемент с id = " + id + " успешно удалён", new User("", ""));
+            } else {
+                return new Response(Response.Status.WARNING, "remove_by_id: Элемента с id = " + id + " не нашлось либо элемент был создан другим пользователем", new User("", ""));
+            }
         } else {
-            return new Response(Response.Status.OK, "remove_by_id: Элемента с id = "+id+" не нашлось либо элемент был создан другим пользователем", new User("", ""));
+            return new Response(Response.Status.ERROR, "remove_by_id: Элемента с id = " + id + " не нашлось", new User("", ""));
         }
     }
 }
